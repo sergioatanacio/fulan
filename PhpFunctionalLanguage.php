7000ff6427436448ae20a0e32776b72651fe720f
Your Name <you@example.com>
2022-02-06 11:42:39 -0500

    Eliminado mas valores del json encode

--- PhpFunctionalLanguage.php
+++ PhpFunctionalLanguage.php
@@ -168,7 +168,7 @@ function operation()
                     def($sendEvalResult,
                         iffn(
                             fn()=>is_array($firstValue) || is_array($secondValue),
-                            fn()=>eval('return unserialize(\''.serialize($firstValue).'\', true) '.$operator.' json_decode(\''.json_encode($secondValue).'\', true);'),
+                            fn()=>eval('return unserialize(\''.serialize($firstValue).'\') '.$operator.' unserialize(\''.serialize($secondValue).'\');'),
                             fn()=>eval('return '.$firstValue.' '.$operator.' '.$secondValue.';')
                         )
                     );
